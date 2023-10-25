0f4baa7293cd74d1aa56d316a31a389eef1099c7
yangkai2001 <2012831771@qq.com>
2023-10-25 23:10:23 +0800

    菜品上传

--- sky-pojo/src/main/java/com/sky/entity/Dish.java
+++ sky-pojo/src/main/java/com/sky/entity/Dish.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Repository;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceimpl.java
@@ -0,0 +1,40 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class DishServiceimpl implements DishService {
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+    @Transactional
+    public void saveWithFlavor(DishDTO dishDTO) {
+          Dish dish=new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+        //向菜品表插入一条数据
+         dishMapper.insert(dish);
+        Long dishId = dish.getId();
+        List<DishFlavor> flavors=dishDTO.getFlavors();
+        if (flavors !=null && flavors.size() >0){
+            flavors.forEach(dishFlavor -> {dishFlavor.setDishId(dishId);});
+            //向口味表中插入n条数据
+            dishFlavorMapper.insertBatch(flavors);
+
+         }
+
+    }
+}
